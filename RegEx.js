25c16be014845969567e4b12c1a5a6d210255efb
pvijaykrishna549 <you@example.com>
2021-06-18 22:16:32 +0530

    Regex Validation For 400088 And 400 088

--- RegEx.js
+++ RegEx.js
@@ -1,11 +1,14 @@
-let PincodeWithoutAlphaSplCharacter = '400088B';
-let pinRegex = RegExp('^[1-9][0-9]{5}$');
-let pinCheck = pinRegex.test(PincodeWithoutAlphaSplCharacter);
-if (pinCheck == true)
+let Pincode = '400088';
+let PincodeWithSpace = '400 088'
+let pinRegex = RegExp('^[1-9][0-9]{2}[ ]?[0-9]{3}$');
+let Pin_Regex = RegExp('^[1-9][0-9]{2}[ ]?[0-9]{3}$');
+let pinCheck = pinRegex.test(Pincode);
+let Pin_Check = Pin_Regex.test(PincodeWithSpace)
+if (pinCheck == true && Pin_Check == true)
 {
-    console.log("Regex Passed for Pincode: " + PincodeWithoutAlphaSplCharacter);
+    console.log("Regex Passed for: " + Pincode +" And " + PincodeWithSpace);
 }
 else 
 {
-    console.log("Regex Failed for PinCode:" + PincodeWithoutAlphaSplCharacter)
+    console.log("Regex Failed for: " + Pincode+ " And "+ PincodeWithSpace);
 }
